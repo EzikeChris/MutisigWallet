f4c8492cb2007d7b78032c9002cc1fdde281db67
Christopher Ezike <synergyaffirm@gmail.com>
2021-07-01 00:04:54 +0100

    added Chai Test

---
+++ MultiSigWallet/test/multi-sig-wallet.js
@@ -0,0 +1,9 @@
+//initialise chai 
+const chai = require('chai');
+chai.use(require('chai-as-promised'));
+// contract artifacts
+const MultiSigWallet = artifacts.require('MultiSigWallet');
+// truffle test 
+contract("MultiSigWallet", account = {
+    
+})
